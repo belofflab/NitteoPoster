833ce319d3b420ecb16ebc29c1250c27d0b457c7
Константин Белов <russianosint@gmail.com>
2023-01-11 01:53:49 -0700

    first commit

--- backend/src/application.py
+++ backend/src/application.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-from src.database.connection import database
+# from src.database.connection import database
 from src.routers import items
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    if not database.is_connected:
-        await database.connect()
+# @app.on_event("startup")
+# async def startup():
+#     if not database.is_connected:
+#         await database.connect()
 
-@app.on_event("shutdown")
-async def shutdown():
-    if database.is_connected:
-        await database.disconnect()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     if database.is_connected:
+#         await database.disconnect()
 
 
 @app.get('/')
