6e3813510e83529066ffb18249471d5e558e54e7
Константин Белов <russianosint@gmail.com>
2023-01-11 02:53:35 -0700

    new

--- backend/app/api/admin/routes.py
+++ backend/app/api/admin/routes.py
@@ -3,4 +3,4 @@ from aiohttp.web import Application
 from . import views
 
 def setup_api_routes(application: Application):
-    application.router.add_post('/api/v1/item', views.public_authorize_admin)
+    application.router.add_post('/api/v1/item', views.public_proceed_item)
