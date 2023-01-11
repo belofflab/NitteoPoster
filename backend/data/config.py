3db3d41ee7bbd27f0ccea207c785cd9db287cd2c
Константин Белов <russianosint@gmail.com>
2023-01-11 02:52:38 -0700

    new

--- backend/data/config.py
+++ backend/data/config.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__name__).resolve().parent
 ENV_FILE = BASE_DIR / '.env'
 
+SECRET_KEY = '1'
 
 if os.path.exists(ENV_FILE):
     load_dotenv(ENV_FILE)
