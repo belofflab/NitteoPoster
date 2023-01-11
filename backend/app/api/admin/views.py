14ea0fa7640c6c5b14fa5eaf72909a49a15c768b
Константин Белов <russianosint@gmail.com>
2023-01-11 03:14:00 -0700

    new

--- backend/app/api/admin/views.py
+++ backend/app/api/admin/views.py
@@ -4,7 +4,7 @@ import json
 from aiohttp import web
 
 async def public_proceed_item(request: web.Request):
-    print(await request.json())
+    print(request)
     return web.Response(text=json.dumps({'req': await request.json()}))
 
 class DecimalEncoder(json.JSONEncoder):
