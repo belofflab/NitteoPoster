e4f33c9d185365c73033ad582a7890e242bddfea
Константин Белов <russianosint@gmail.com>
2023-01-19 09:47:53 -0700

    first

--- backend/app/api/admin/manager.py
+++ backend/app/api/admin/manager.py
@@ -38,5 +38,7 @@ class DBManager:
             """Получение доступных обменов"""
             self.connection.cur.execute(f"""SELECT * FROM {self.prefix}_napobmens WHERE status = 1""")
             napobmens = [el for el in self.connection.cur]
-            text = "".join([f"{self.get_valuts(napobmen[1])[4]} -> {self.get_valuts(napobmen[2])[4]} {napobmen[3] if napobmen[3] != '1' else napobmen[4]}" for napobmen in napobmens])
+            text ="1"
+            # text = "".join([f"{self.get_valuts(napobmen[1])[4]} -> {self.get_valuts(napobmen[2])[4]} {napobmen[3] if napobmen[3] != '1' else napobmen[4]}" for napobmen in napobmens])
+            print([self.get_valuts(napobmen[1])[4] for napobmen in napobmens])
             return text
