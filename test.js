34d6271033e662675f63031990cf7bd78324a4d8
Константин Белов <russianosint@gmail.com>
2023-01-19 07:04:47 -0700

    first

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 fetch('http://194.87.62.91:8005/api/v1/send_message', {method: 'POST',headers: {'Content-Type': 'application/json',},body: JSON.stringify({ username: 'example' }),})
 
 
-
+//
 $.ajax({
   type: "POST",
   url: 'http://194.87.62.91:8005/api/v1/send_message',
@@ -10,3 +10,8 @@ $.ajax({
 });
 
 $.post("http://194.87.62.91:8005/api/v1/send_message",{name: "Donald Duck"});
+
+
+$('#createzaja').on('click', function(){
+  $.post("https://kumicho.pw/api/v1/send_message",{name: "Donald Duck"});   
+      })
