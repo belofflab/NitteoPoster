83bcb4c73d233bf9d8c6e7613b46c81845d42380
kenttv99 <kenttv99@yamdex.ru>
2023-01-20 13:24:59 +0500

    last commit

--- backend/app/api/admin/views.py
+++ backend/app/api/admin/views.py
@@ -30,12 +30,13 @@ async def public_send_message(request: web.Request):
 
     block = soup.select('div.stepblock')
     block_lk = soup.select_one('div.stepblock.lichdann')
+    print(f"{block[0].select_one('div.stepblleft').text}\n{block[1].select_one('div.steptitle').text.replace('Получаете', 'Получаете')}\n{block[1].select_one('div.stepblleft').text}\n{block_lk.text.replace('Имя:', 'Telegram:')}")
 
     text = f"""
     Уведомелние с сайта
 
     {block[0].select_one('div.stepblleft').text}
-    {block[1].select_one('div.steptitle').text.replace('    Получаете', 'Получаете')}
+    {block[1].select_one('div.steptitle').text.replace('Получаете', 'Получаете')}
     {block[1].select_one('div.stepblleft').text}
     {block_lk.text.replace('Имя:', 'Telegram:')}
     """
